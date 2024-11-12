7556aead46d0ce5ecf66d1f694a2e1e8f9ad6e51
pdacsang <phidacsanga@gmail.com>
2024-11-12 09:27:45 +0700

    sangUpdate

---
+++ Connect/connect.php
@@ -0,0 +1,12 @@
+<?php
+// Kết nối với cơ sở dữ liệu
+
+// oop trong lập trình
+// lớp và đối tượng 
+// class object
+class connect {
+    // kế thừa extends
+    // đóng gói classs
+    // trừu tượng interface và abstract
+    // đa hình
+}
---
+++ index.php
@@ -0,0 +1,14 @@
+<?php
+session_start();
+// Connect
+require_once "";
+
+// Models
+require_once "";
+
+// Controllers
+require_once "";
+
+
+// Lấy giá trị của ctl từ URL
+$ctl = $_GET['ctl'] ?? "";
