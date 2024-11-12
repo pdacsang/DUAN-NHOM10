02b75f9f3d1fe79e59efb3723c75e25cbd9fa957
vunv-15 <nguyenvu15309@gmail.com>
2024-11-12 11:02:44 +0700

    lanmot

--- Connect/connect.php
+++ Connect/connect.php
@@ -1,12 +1,12 @@
 <?php
-// Kết nối với cơ sở dữ liệu
-
-// oop trong lập trình
-// lớp và đối tượng 
-// class object
-class connect {
-    // kế thừa extends
-    // đóng gói classs
-    // trừu tượng interface và abstract
-    // đa hình
-}
+    // kết nối cơ sở dữ liệu
+    // oop trong lập trình
+    // lớp và đối tượng
+    // class object
+    class connect{
+        // kế thừa extends
+        // đóng gói class
+        // trừu tượng interface và abstract
+        // đa hình
+    }
+?>
